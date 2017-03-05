6b3d76e460103ddb7959bed4c56f4c2e6f784cd3
Kiela <kiela.agustin@gmail.com>
2017-03-05 13:13:39 +0800

    Revert "Revert "Added the Background""
    This reverts commit 3f209d657961b1b2ae40ab73d17f1b8037e99fa1.

--- Kielacode/src/Face.java
+++ Kielacode/src/Face.java
@@ -17,6 +17,7 @@ public class Face {
 	}
 	
 	public void update(String faceString){
+		System.out.println("Updating face...");
 		if(faceString.contains(" ")){
 			System.out.println("Compiling face...");
 			String[] draw = faceString.split(" ");
@@ -46,9 +47,21 @@ public class Face {
 			this.body = "0";
 			this.others = "1";
 			break;
-		case "excited":
+		case "shocked":
 			this.eyes = "0";
 			this.mouth = "3";
+			this.body = "0";
+			this.others = "0";
+			break;
+		case "happy":
+			this.eyes = "0";
+			this.mouth = "0";
+			this.body = "0";
+			this.others = "0";
+			break;
+		case "annoyed":
+			this.eyes = "2";
+			this.mouth = "2";
 			this.body = "0";
 			this.others = "0";
 			break;
